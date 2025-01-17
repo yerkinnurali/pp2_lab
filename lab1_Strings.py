a4093308425de05f48d96ffaf2169cffaa7aea29
Your Name <your_email@whatever.com>
2025-01-17 21:35:48 +0500

    f

---
+++ lab1_Strings.py
@@ -0,0 +1,33 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+a = "Hello"
+print(a)
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+b = "Hello, World!"
+print(b[2:5])
+print(b[2:])
+print(b[-5:-2])
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("H", "J"))
+print(a.split(",")) # returns ['Hello', ' World!']
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+txt = "We are the so-called \"Vikings\" from the north."
