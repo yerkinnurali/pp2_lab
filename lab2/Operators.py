4351fab00a59e37267cb5dfc2b3f53a763be5366
Nurali Yerkin <n_yerkin@kbtu.kz>
2025-01-22 14:36:22 +0500

    f

---
+++ lab2/Operators.py
@@ -0,0 +1,32 @@
+#python arithmetic operators
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 % 5)
+print(10 ** 5)
+print(10 // 5)
+#python assignment operations
+x = 5
+x += 3
+x -= 3
+x *= 3
+x /= 3
+x %= 3
+x //= 3
+#python comparison operations
+x=0
+y=0
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+#Python Logical Operators
+print(x < 5 and  x < 10)
+print(	x < 5 or x < 4)
+print(	not(x < 5 and x < 10))
+#Python Bitwise Operators
+print(x & y)
+print(	x | y)
+print(	x ^ y)
+print(	~x)
