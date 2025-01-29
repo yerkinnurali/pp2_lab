1de00b1c5d7f1d37a9c8d1cdc99a15ba77f40417
Nurali Yerkin <n_yerkin@kbtu.kz>
2025-01-29 13:41:00 +0500

    f

---
+++ lab3/functions1.1.py
@@ -0,0 +1,4 @@
+def ounces(x):
+    return 28.3495231 * x
+a=int(input("Gramm:"))
+print("Ounces:",ounces(a))
---
+++ lab3/functions1.2.py
@@ -0,0 +1,4 @@
+def a(x):
+    return  (5 / 9) * (x - 32)
+x=int(input("Fahrenhait:"))
+print("Celcium:",a(x))
