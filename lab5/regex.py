845d951940f3253e092701bbc69e0d2977316af5
Nurali Yerkin <n_yerkin@kbtu.kz>
2025-02-23 16:13:04 +0500

    f

--- lab5/regex.py
+++ lab5/regex.py
@@ -8,11 +8,11 @@ for row in data:
  print(a)
 print("Task 2")
 for row in data:
- a=re.findall("a.*b{2,3}",row)
+ a=re.findall("ab{2,3}",row)
  print(a)
 print("Task 3")
 for row in data:
-    a=re.findall("[a-z]_[a-z]",row)
+    a=re.findall("[a-z]+_[a-z]+",row)
     print(a)
 print("Task 4")
 for row in data:
@@ -27,7 +27,21 @@ for row in data:
     a=re.sub(r"[ ,.]",":",row)
     print(a)
 print("Task 7")
-
 for row in data:
-    a=re.sub(r"_[a-z]","A-Z",row)
+    a=re.sub(r"_([a-z])",lambda a:a.group(1).upper(),row)
     print(a)
+print("Task 8")
+for row in data:
+    a=re.sub(r"([A-Z])",r"@\1",row)
+    b=re.split("@",a)
+    print(b)
+print("Task 9")
+for row in data:
+    a=re.sub(r"([A-Z])",r" \1",row)
+    print(a)
+print("Task 10")
+for row in data:
+    a=re.sub(r"([A-Z])",r"_\1",row)
+    b=re.sub(r"_([A-Z])",lambda c:"_"+c.group(1).lower(),a)
+    print(b)
+
