6eb6498ed48a66585d1a930e172614325b7fc2b1
Nurali Yerkin <n_yerkin@kbtu.kz>
2025-04-15 21:58:04 +0500

    f

---
+++ lab10/phonebook.py
@@ -0,0 +1,56 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="phonebook_db",
+    user="postgres",
+    password="eca11aec3e2b"
+)
+
+cur = conn.cursor()
+
+while True:
+    print("Phonebook")
+    print("1. Add name and phone")
+    print("2. Show all")
+    print("3. Exit")
+    print("4. Update info")
+    print("5. Delete info")
+    choice = input("Choose number: ")
+
+    if choice == "1":
+        name = input("Enter name: ")
+        phone = input("Enter phone: ")
+        cur.execute("INSERT INTO PhoneBook (first_name, phone) VALUES (%s, %s);", (name, phone))
+        conn.commit()
+        print("Info added.")
+    elif choice == "2":
+        cur.execute("SELECT * FROM PhoneBook;")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    elif choice == "3":
+        cur.close()
+        conn.close()
+        print("Bye!")
+        break
+    elif choice == "4":
+        name = input("Write old name: ")
+        new_name = input("New name (or nothing): ")
+        new_phone = input("New phone (or nothing): ")
+
+        if new_name:
+            cur.execute("UPDATE PhoneBook SET first_name = %s WHERE first_name = %s;", (new_name, name))
+        if new_phone:
+            cur.execute("UPDATE PhoneBook SET phone = %s WHERE first_name = %s;", (new_phone, new_name or name))
+
+        conn.commit()
+        print("Info updated.")
+    elif choice == "5":
+        name = input("Enter name to delete: ")
+        phone = input("Enter phone to delete: ")
+        cur.execute("DELETE FROM PhoneBook WHERE first_name = %s AND phone = %s;", (name, phone))
+        conn.commit()
+        print("Info deleted.")
+    else:
+        print("Wrong number. Try again.")
---
+++ lab10/snake.py
@@ -0,0 +1,114 @@
+import pygame, sys, random, psycopg2
+
+#connect to database
+conn = psycopg2.connect(
+    host="localhost",
+    database="snake_game.db",
+    user="postgres",
+    password="eca11aec3e2b"
+)
+cur = conn.cursor()
+
+# Player name
+username = input("Your name: ")
+
+# search player
+cur.execute("SELECT id, score, level FROM players WHERE username = %s;", (username,))
+player = cur.fetchone()
+
+#
+if player is None:
+    cur.execute("INSERT INTO players (username) VALUES (%s) RETURNING id, score, level;", (username,))
+    user_id, score, level = cur.fetchone()
+    conn.commit()
+    print("New user created.")
+else:
+    user_id, score, level = player
+    print("Welcome,", username)
+
+# speed
+speed = 10 + (level - 1) * 2
+
+
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 20)
+
+snake = [(100, 100)]
+direction = (20, 0)
+
+
+def make_food():
+    x = random.randint(0, 39) * 20
+    y = random.randint(0, 29) * 20
+    return x, y
+
+
+food = make_food()
+
+
+def game_over():
+    cur.execute("UPDATE players SET score = %s, level = %s WHERE id = %s;", (score, level, user_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    pygame.quit()
+    print("Game Over. Score:", score)
+    sys.exit()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != (0, 20):
+                direction = (0, -20)
+            if event.key == pygame.K_DOWN and direction != (0, -20):
+                direction = (0, 20)
+            if event.key == pygame.K_LEFT and direction != (20, 0):
+                direction = (-20, 0)
+            if event.key == pygame.K_RIGHT and direction != (-20, 0):
+                direction = (20, 0)
+            if event.key == pygame.K_p:
+                cur.execute("UPDATE players SET score = %s, level = %s WHERE id = %s;", (score, level, user_id))
+                conn.commit()
+                print("Game paused. Press P again.")
+                paused = True
+                while paused:
+                    for e in pygame.event.get():
+                        if e.type == pygame.KEYDOWN and e.key == pygame.K_p:
+                            paused = False
+
+
+    head = (snake[0][0] + direction[0], snake[0][1] + direction[1])
+
+    if head[0] < 0 or head[0] >= 800 or head[1] < 0 or head[1] >= 600 or head in snake:
+        game_over()
+
+    snake = [head] + snake
+
+    if head == food:
+        score += 1
+        if score % 3 == 0:
+            level += 1
+            speed += 2
+        food = make_food()
+    else:
+        snake.pop()
+
+
+    screen.fill((0, 0, 0))
+    for s in snake:
+        pygame.draw.rect(screen, (0, 255, 0), (*s, 20, 20))
+    pygame.draw.rect(screen, (255, 0, 0), (*food, 20, 20))
+
+
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    level_text = font.render(f"Level: {level}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+    screen.blit(level_text, (10, 30))
+
+    pygame.display.flip()
+    clock.tick(speed)
